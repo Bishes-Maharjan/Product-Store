513f224b23f802a0a67f3779e48076cd64c55208
Bishes Maharjan <maharjanbichheach0909@gmail.com>
2025-11-27 08:57:33 +0545

    finxing next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["cdn.dummyjson.com"], // add the external domain here
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
